a9e0227106611f14c026a300c0b5f629d1b1e8b2
Martin Pintos <102275218+MartinPintos677@users.noreply.github.com>
2023-10-13 16:21:26 -0300

    Atualizações

---
+++ src/main/java/br/com/martinpintos/todolist/errors/ExceptionHandlerController.java
@@ -0,0 +1,8 @@
+package br.com.martinpintos.todolist.errors;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class ExceptionHandlerController {
+  
+}
